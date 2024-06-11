e46275564071827e75e9e49ab563d46d57d271eb
Mohit Jangid <122960902+mohit070798@users.noreply.github.com>
2024-06-11 23:44:34 +0530

    Add files via upload

--- new/scripts.js
+++ new/scripts.js
@@ -36,13 +36,13 @@ $(document).ready(function () {
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 3,
         },
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
         },
       },
     ],
@@ -73,112 +73,4 @@ $(document).ready(function () {
   dynamicNumberElement.forEach((element) => {
     element.setAttribute("data-number", "#" + ++number);
   });
-
-  // Typewriter effect for slider
-  function typeText(element, text) {
-    element.innerText = '';
-    let index = 0;
-
-    function type() {
-      if (index < text.length) {
-        element.innerText += text.charAt(index);
-        index++;
-        setTimeout(type, 100); // Adjust typing speed here
-      }
-    }
-
-    type();
-  }
-
-  function initTypingEffect() {
-    const currentSlide = $('.slider-banner').slick('slickCurrentSlide');
-    const textElement = $('.slider-banner .slick-slide[data-slick-index="' + currentSlide + '"] #typing-text');
-    const textToType = textElement.data('text');
-
-    typeText(textElement[0], textToType);
-  }
-
-  $('.slider-banner').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-    const textElement = $('.slider-banner .slick-slide[data-slick-index="' + nextSlide + '"] #typing-text');
-    const textToType = textElement.data('text');
-
-    typeText(textElement[0], textToType);
-  });
-
-  // Initialize typing effect on page load
-  // initTypingEffect();
-  
-  document.getElementById('indigenousFeatures').addEventListener('click', () => {
-      document.getElementById('features').scrollIntoView({ behavior: 'smooth' });
-  });
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-  const textElement = document.getElementById('typing-text');
-  const textToType = textElement.dataset.text;
-  textElement.innerText = '';
-
-  let index = 0;
-
-  function typeText() {
-      if (index < textToType.length) {
-          textElement.innerText += textToType.charAt(index);
-          index++;
-          setTimeout(typeText, 100); // Adjust typing speed here
-      }
-  }
-
-  typeText();
-});
-
-$(".banner-button").on("click", function () {
-  var currentSlide = $(".slider-banner").slick("slickCurrentSlide");
-  var targetSection;
-
-  switch (currentSlide) {
-    case 0:
-      targetSection = "#business"; // Replace with the ID of your first section
-      break;
-    case 1:
-      targetSection = "#product"; // Replace with the ID of your second section
-      break;
-    case 2:
-      targetSection = "#features"; // Replace with the ID of your third section
-      break;
-    default:
-      targetSection = "#"; // Default to first section
-      break;
-  }
-
-  $(targetSection)[0].scrollIntoView({ behavior: "smooth" });
-});
-
-
-
-// function typeText(element, text) {
-//   element.innerText = '';
-//   let index = 0;
-
-//   function type() {
-//       if (index < text.length) {
-//           if (text.charAt(index) === ' ') {
-//               // If the character is a space, add a non-breaking space
-//               element.innerHTML += '&nbsp;';
-//           } else {
-//               element.innerText += text.charAt(index);
-//           }
-//           index++;
-//           setTimeout(type, 100); // Adjust typing speed here
-//       }
-//   }
-
-//   type();
-// }
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   const textElement = document.getElementById('typing-text');
-//   const textToType = textElement.dataset.text;
-//   textElement.innerText = '';
-
-//   typeText(textElement, textToType);
-// });
